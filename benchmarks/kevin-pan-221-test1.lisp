0bb2e241ce2401ca4a563187a7fe78fd872f74c0
Kevin Pan <kevin.yonghao.pan@gmail.com>
2025-12-03 17:08:41 -0500

    fix parens

--- benchmarks/kevin-pan-221-test1.lisp
+++ benchmarks/kevin-pan-221-test1.lisp
@@ -5,4 +5,4 @@
 (let ((a (sum 15)))
     (let ((b (+ (sum 15) (sum 10))))
         (let ((c (+ (+ (sum 15) (sum 10)) (sum 20))))
-            (print (+ (+ a b)) c))))
+            (print (+ (+ a b) c)))))
